adf839f54676a75291f86d4cdff3a8434916f36b
sebtn <sdebedout@gmail.com>
2017-06-20 18:40:42 -0400

    Using reducers and smart containers to render

--- app/app.js
+++ app/app.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
 import  '././styles/app.scss' 
+import BookApp from './components/book-app'
+import reducers from './reducers'
 
+const createStoreWithMiddleware = applyMiddleware()(createStore)
+ReactDOM.render(
+	<Provider store={createStoreWithMiddleware(reducers)} >
+		<BookApp />
+	</Provider>
 
-ReactDOM.render(<VidApp />, document.getElementById('root'))
+	, document.getElementById('root'))
